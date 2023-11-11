a60b38adba329061c408e8767bd874a8d5dbb54d
Weston Vincze <westonvincze@gmail.com>
2023-11-10 23:49:12 -0500

    Adds game development project data and small ui improvements

--- scenes/ProjectsScene/ProjectsScene.tsx
+++ scenes/ProjectsScene/ProjectsScene.tsx
@@ -7,8 +7,13 @@ export const ProjectsScene = (): JSX.Element => {
   return (
     <Container>
       <AnimatedContent>
+        <h1>Web Development</h1>
         {ProjectsContent.websites.map((website, i) => (
           <ProjectCard {...website} key={i} />
+        ))}
+        <h1>Game Development</h1>
+        {ProjectsContent.games.map((game, i) => (
+          <ProjectCard {...game} key={i} />
         ))}
       </AnimatedContent>
     </Container>
