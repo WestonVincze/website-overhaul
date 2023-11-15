f5356330186d8978a7ad30a6d6d207d344fad4eb
Weston Vincze <westonvincze@gmail.com>
2023-11-14 22:40:14 -0500

    jest setup and test for AnimatedContent

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,12 @@
 import "@testing-library/jest-dom";
+
+jest.mock('../AppStateProvider', () => ({
+  useAppState: () => ({
+    appState: {},
+    isReady: true,
+  }),
+}));
+
+jest.mock('@xstate/react', () => ({
+  useSelector: () => true,
+}));
