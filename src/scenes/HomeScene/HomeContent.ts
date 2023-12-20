663b50b3320a2c28cb5cc8b018bcca3bf26d373b
Weston Vincze <westonvincze@gmail.com>
2023-12-19 23:37:11 -0500

    Updates placeholder intro content with a more concise version

---
+++ src/scenes/HomeScene/HomeContent.ts
@@ -0,0 +1,6 @@
+const intro =
+  "I craft immersive web applications and games with a strong emphasis on delivering exceptional user experiences. I'm on a mission to join a challenge-driven team and collaborate on solutions that captivate and impress.";
+
+export const HomeContent = {
+  intro,
+};
