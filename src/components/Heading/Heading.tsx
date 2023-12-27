7b260d20306114880d822f21edec88213bcd068c
Weston Vincze <westonvincze@gmail.com>
2023-12-27 17:12:59 -0500

    Adds DarkModeSwitch to heading and adjusts layout for responsiveness

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,18 +1,24 @@
 import { FolderTabs } from "@components/FolderTabs";
 import styles from "./Heading.module.css";
 import { SocialIcons } from "@components/SocialIcons";
+import { DarkModeSwitch } from "@components/DarkModeSwitch";
 
 export const Heading = () => {
   return (
     <header className={styles.heading}>
-      <SocialIcons />
-      <FolderTabs
-        tabs={[
-          { path: "/", text: "Home" },
-          { path: "/resume", text: "Resume" },
-          { path: "/projects", text: "Projects" },
-        ]}
-      />
+      <span className={styles.buttons}>
+        <DarkModeSwitch />
+        <SocialIcons />
+      </span>
+      <span className={styles.tabs}>
+        <FolderTabs
+          tabs={[
+            { path: "/", text: "Home" },
+            { path: "/resume", text: "Resume" },
+            { path: "/projects", text: "Projects" },
+          ]}
+        />
+      </span>
     </header>
   );
 };
