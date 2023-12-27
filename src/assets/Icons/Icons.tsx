213126ebe10528d5e3ac822e22b54b0292316246
Weston Vincze <westonvincze@gmail.com>
2023-12-27 17:06:15 -0500

    Adds sun and moon icons

--- src/assets/Icons/Icons.tsx
+++ src/assets/Icons/Icons.tsx
@@ -994,6 +994,46 @@ export const Icons: Record<string, IconProps> = {
       </svg>
     ),
   },
+  Sun: {
+    title: "Sun Icon",
+    icon: () => (
+      <svg
+        className="w-6 h-6 text-gray-800 dark:text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentcolor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M10 3V1m0 18v-2M5.05 5.05 3.636 3.636m12.728 12.728L14.95 14.95M3 10H1m18 0h-2M5.05 14.95l-1.414 1.414M16.364 3.636 14.95 5.05M14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+        />
+      </svg>
+    ),
+  },
+  Moon: {
+    title: "Moon Icon",
+    icon: () => (
+      <svg
+        className="w-6 h-6 text-gray-800 dark:text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentcolor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M8.509 5.75c0-1.493.394-2.96 1.144-4.25h-.081a8.5 8.5 0 1 0 7.356 12.746A8.5 8.5 0 0 1 8.509 5.75Z"
+        />
+      </svg>
+    ),
+  },
 };
 
 export type IconName = keyof typeof Icons;
