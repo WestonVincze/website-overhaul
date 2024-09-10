12a98511f889807523fe5c420fd5ce4f038e90ef
Weston Vincze <westonvincze@gmail.com>
2024-09-10 01:38:27 -0400

    📝 Update experience in summary

--- src/data/Resume/Resume.ts
+++ src/data/Resume/Resume.ts
@@ -5,7 +5,7 @@ import { technicalSkills } from "./TechnicalSkills";
 import { workExperience } from "./WorkExperience";
 
 const intro =
-  "I am a self-driven frontend web developer backed by over 5 years of professional experience. I specialize in JavaScript, including a plethora of libraries and frameworks, with a strong proficiency in React and TypeScript.";
+  "I am a self-driven frontend web developer backed by over 7 years of professional experience. I specialize in JavaScript, including a plethora of libraries and frameworks, with a strong proficiency in React and TypeScript.";
 
 export const Resume: ResumeFormat = {
   intro,
