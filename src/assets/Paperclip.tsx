cf7ed129ea022354759704403709830b0ad33ada
Weston Vincze <westonvincze@gmail.com>
2024-09-15 17:12:47 -0400

    🍱 Update icons and add paperclip svg

---
+++ src/assets/Paperclip.tsx
@@ -0,0 +1,28 @@
+export const Paperclip = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={49}
+    height={132}
+    viewBox="0 0 49 132"
+    fill="none"
+  >
+    <path
+      fill="url(#a)"
+      d="M7.685 68.015a2 2 0 1 0-4 0h4ZM45 24.5v83.843h4V24.5h-4ZM7.685 108.343V68.015h-4v40.328h4ZM22.258 4H24.5V0h-2.242v4Zm0-4C9.965 0 0 9.965 0 22.258h4C4 12.175 12.175 4 22.258 4V0ZM3.685 108.343C3.685 120.856 13.83 131 26.343 131v-4c-10.305 0-18.658-8.353-18.658-18.657h-4Zm41.315 0C45 118.647 36.647 127 26.343 127v4C38.856 131 49 120.856 49 108.343h-4ZM49 24.5C49 10.969 38.031 0 24.5 0v4C35.822 4 45 13.178 45 24.5h4Z"
+    />
+    <defs>
+      <linearGradient
+        id="a"
+        x1={24.5}
+        x2={24.5}
+        y1={2}
+        y2={129}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#555" />
+        <stop offset={0.323} stopColor="#B8B8B8" />
+        <stop offset={1} stopColor="#555" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
