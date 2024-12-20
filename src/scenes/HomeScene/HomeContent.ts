eec6774ad49f7fcde0ea77be63d752af6bcf8167
Weston Vincze <westonvincze@gmail.com>
2024-12-20 01:39:43 -0500

    🚨 Pretty Fix

--- src/scenes/HomeScene/HomeContent.ts
+++ src/scenes/HomeScene/HomeContent.ts
@@ -1,7 +1,6 @@
 const intro =
   "I'm a self-driven web developer with over 7 years of experience crafting immersive digital experiences. Based in Ontario, Canada, I specialize in JavaScript libraries and frameworks, particularly React and TypeScript. I'm a curios creator who enjoys experimenting with new technology, participating in game jams, and crafting innovative experiences for the web. I am driven by an insatiable thirst for knowledge and love to share what I've learned with other curious developers.";
 
-
 export const HomeContent = {
   intro,
 };
